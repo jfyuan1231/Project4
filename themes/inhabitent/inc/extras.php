4f309d659d28bbda17fde3127d86d3111a2b87f9
Junfeng <sameoneagain11@gmail.com>
2020-01-07 08:31:35 -0800

    add redstart-master

--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -25,3 +25,29 @@ function red_starter_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', 'red_starter_body_classes' );
+
+// change wordpress logo
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/inhabitent-logo-full.svg);
+		height:65px;
+		width:320px;
+		background-size: 320px 65px;
+		background-repeat: no-repeat;
+        	padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+// redirect logo url 
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'inhabitent';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
