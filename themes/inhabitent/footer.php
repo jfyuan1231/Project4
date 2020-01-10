8d694dd15ec6923cabdb6cfdbb8274e587e92964
Junfeng <sameoneagain11@gmail.com>
2020-01-09 21:52:52 -0800

    functionality plugin, product custom post type, generate product type taxonomy, custom field suite, create block template, inhabitent block plugin

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -7,14 +7,36 @@
 
 ?>
 
-			</div><!-- #content -->
-
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<div class="contact-info">
+						<h2>Contact Info</h2>
+						<p>
+							<a href="info@inhabitent.com">info.inhabitent.com</a> <br>
+							778-456-7891
+						</p>
+					</div>
+
+					<div class="open-hour">
+						<h2>Business Hours</h2>
+						<p>
+							Monday-Friday: 9am to 5pm <br>
+							Saturday: 10am to 2pm <br>
+							Sunday: Closed
+						</p>
+					</div>
+
+					<div class="logo-img">
+						<a href="<?php echo esc_url( 'http://localhost:8888/wp4/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					</div>
+
+					<div class="copyright">
+						<p>&#169; COPYRIGHT 2019 INHIBITENT</p>
+					</div>
+
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
-		</div><!-- #page -->
+		
 
 		<?php wp_footer(); ?>
 
